ac0e48ceba7117e091d810887b89893746b37bbc
zach beecher <zachbeecher@gmail.com>
2017-03-31 16:53:52 -0700

    add final touches still need to fix issues

--- app/app.php
+++ app/app.php
@@ -18,64 +18,44 @@
     Request::enableHttpMethodParameterOverride();
 
     $app->get("/", function() use ($app) {
-      return $app['twig']->render('index.html.twig');
+      return $app['twig']->render('index.html.twig', array('stylists'=> Stylist::getAll()));
     });
-
-    // $app->get("/clients", function() use ($app) {
-    //     return $app['twig']->render('clients.html.twig', array('clients' => Client::getAll()));
-    // });
-
-//     $app->post("/clients", function() use ($app) {
-//         $description = $_POST['description'];
-//         $client_id = $_POST['client_id'];
-//         $task = new Client($description, $id = null, $client_id);
-//         $task->save();
-//         $client = Stylist::find($client_id);
-//         return $app['twig']->render('client.html.twig', array('client' => $client, 'clients' => $client->getClients()));
-//     });
-//
-//     $app->get("/stylists", function() use ($app) {
-//         return $app['twig']->render('stylists.html.twig', array('stylists' => Stylist::getAll()));
-//     });
-//
-//     $app->post("/stylists", function() use ($app) {
-//         $client = new Stylist($_POST['name']);
-//         $client->save();
-//         return $app['twig']->render('index.html.twig', array('stylists' => Stylist::getAll()));
-//     });
-//
-//     $app->get("/stylists/{id}/edit", function($id) use ($app) {
-//     $client = Stylist::find($id);
-//     return $app['twig']->render('client_edit.html.twig', array('client' => $client));
-//     });
-//
-//     $app->patch("/stylists/{id}", function($id) use ($app) {
-//     $name = $_POST['name'];
-//     $client = Stylist::find($id);
-//     $client->update($name);
-//     return $app['twig']->render('client.html.twig', array('client' => $client, 'clients' => $client->getClients()));
-// });
-//
-//     $app->get("/stylists/{id}", function($id) use ($app) {
-//     $client = Stylist::find($id);
-//     return $app['twig']->render('client.html.twig', array('client' => $client, 'clients' => $client->getClients()));
-//     });
-//
-//     $app->delete("/stylists/{id}", function($id) use ($app) {
-//     $client = Stylist::find($id);
-//     $client->delete();
-//     return $app['twig']->render('index.html.twig', array('stylists' => Stylist::getAll()));
-//     });
-//
-//     $app->post("/delete_stylists", function() use ($app) {
-//         Stylist::deleteAll();
-//         return $app['twig']->render('index.html.twig');
-//     });
-//
-//     $app->post("/delete_clients", function() use ($app) {
-//         Client::deleteAll();
-//         return $app['twig']->render('index.html.twig');
-//     });
+    $app->post("/stylists", function() use ($app) {
+      $stylist = new Stylist($_POST['name']);
+      $stylist->save();
+      return $app['twig']->render('index.html.twig', array('stylists' => Stylist::getAll()));
+    });
+    $app->get("/clients", function() use ($app) {
+        return $app['twig']->render('clients.html.twig', array('clients' => Client::getAll()));
+    });
+    $app->post('/delete_stylists', function() use ($app) {
+        Stylist::deleteAll();
+        Client::deleteAll();
+        return $app['twig']->render('index.html.twig');
+    });
+    $app->get("/stylists/{id}", function($id) use ($app) {
+        $stylist = Stylist::find($id);
+        return $app['twig']->render('stylists.html.twig', array('stylist' => $stylist,
+        'clients' => $stylist->getClients()));
+    });
+    $app->post("/client", function() use ($app) {
+        $name = $_POST['name'];
+        $stylist_id = $_POST['stylist_id'];
+        $client = new Client($name, $stylist_id);
+        $client->save();
+        $stylist = Stylist::find($stylist_id);
+        return $app['twig']->render('stylists.html.twig', array('stylist' => $stylist, 'clients' => $stylist->getClients()));
+    });
+    $app->get("/seeall", function() use ($app){
+        $stylist = Stylist::getAll();
+        $client = Client::getAll();
+        return $app['twig']->render('seeall.html.twig', array('stylists'=> $stylist, 'clients' => $client));
+    });
+    $app->delete("/stylists_edit/{id}", function($id) use ($app) {
+      $stylist = Stylist::find($id);
+      $stylist->delete();
+      return $app['twig']->render('index.html.twig', array('stylists' => Stylist::getAll()));
+    });
 
 
     return $app;
---
+++ hair_salon.sql
@@ -0,0 +1,88 @@
+-- phpMyAdmin SQL Dump
+-- version 4.4.10
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost:8889
+-- Generation Time: Apr 01, 2017 at 01:44 AM
+-- Server version: 5.5.42
+-- PHP Version: 5.6.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hair_salon`
+--
+CREATE DATABASE IF NOT EXISTS `hair_salon` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `hair_salon`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `clients`
+--
+
+CREATE TABLE `clients` (
+  `id` bigint(20) unsigned NOT NULL,
+  `name` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stylists`
+--
+
+CREATE TABLE `stylists` (
+  `id` bigint(20) unsigned NOT NULL,
+  `name` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `stylists`
+--
+
+INSERT INTO `stylists` (`id`, `name`) VALUES
+(42, 'hey');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `clients`
+--
+ALTER TABLE `clients`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Indexes for table `stylists`
+--
+ALTER TABLE `stylists`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `clients`
+--
+ALTER TABLE `clients`
+  MODIFY `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `stylists`
+--
+ALTER TABLE `stylists`
+  MODIFY `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=43;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
